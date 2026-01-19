a05da322df29fd272dcff1e647c8992745b77398
Brayan <bittolo.8bit@gmail.com>
2026-01-19 18:35:09 +0100

    putting cards in html

--- script.js
+++ script.js
@@ -3,36 +3,51 @@ const teamMembers = [
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    img: "img/male1.png",
   },
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png"
+    img: "img/female1.png",
   },
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+    img: "img/male2.png",
   },
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png"
+    img: "img/female2.png",
   },
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png"
+    img: "img/male3.png",
   },
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png"
-  }
+    img: "img/female3.png",
+  },
 ];
+
+// selectors
+let teamMemberCards = document.getElementById("team-member-cards");
+
+for (const member of teamMembers) {
+  teamMemberCards.innerHTML += `
+        <div>
+            <img src="${member.img}" alt="">
+            <div>
+                <h2>${member.name}</h2>
+                <p>${member.role}</p>
+                <p><a href="mailto:${member.email}">${member.email}</a></p>
+            </div>
+        </div>`;
+}
